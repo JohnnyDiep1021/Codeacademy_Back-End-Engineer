09da9b6caadb41590329ee00d59148b699b62c81
Diep Ly Bao Long <longdiep10102001@gmail.com>
2022-01-05 01:02:13 -0500

    Update_050122_api-quote-project_add-update-feature

--- quote-api-project/public/update-quote.js
+++ quote-api-project/public/update-quote.js
@@ -2,18 +2,20 @@ const updateButton = document.getElementById("update-quote");
 const newQuoteContainer = document.getElementById("new-quote");
 
 updateButton.addEventListener("click", () => {
+  const id = document.getElementById("id").value;
   const quote = document.getElementById("quote").value;
   const person = document.getElementById("person").value;
 
-  fetch(`/api/quotes?quote=${quote}&person=${person}`, {
+  fetch(`/api/quotes/${id}?quote=${quote}&person=${person}`, {
     method: "PUT",
   })
     .then((response) => response.json())
-    .then(({ quote }) => {
+    .then((quote) => {
       const newQuote = document.createElement("div");
+      // console.log(quote);
       newQuote.innerHTML = `
     <h3>Congrats, your quote was updated!</h3>
-    <div class="quote-text">${quote.quote}</div>
+    <div class="quote-text"><span>${quote.id}.</span> ${quote.quote}</div>
     <div class="attribution">- ${quote.person}</div>
     <p>Go to the <a href="index.html">home page</a> to request and view all quotes.</p>
     `;
