deb53f9c767d6805de81e2fd13fe4b424498ac45
Diep Ly Bao Long <longdiep10102001@gmail.com>
2022-05-12 11:17:40 -0400

    Update_120522_handle-CORS

--- personal-budget-project/server.js
+++ personal-budget-project/server.js
@@ -8,6 +8,16 @@ const PORT = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  // '*' allow to access all domains
+  // allow any domains to send the request
+  res.setHeader(`Access-Control-Allow-Origin`, `*`);
+  res.setHeader(
+    `Access-Control-Allow-Headers`,
+    `Origin, X-Requested-With, Content-Type, Accept, Authorization`
+  );
+  res.setHeader(`Access-Control-Allow-Methods`, `GET, POST, PATCH, DELETE`);
+});
 const apiRouter = require("./src/server/api");
 app.use("/api", apiRouter);
 
